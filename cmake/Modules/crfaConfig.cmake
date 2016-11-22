023005fddd19ec3a1da3643a8d8a075836b19629
csrichter <csrichter@hotmail.de>
2016-11-22 18:54:29 +0100

    3 uni-tag (dienstag): basic color text, quadRX (slow),nPorts,CT, TODO:AF

---
+++ cmake/Modules/crfaConfig.cmake
@@ -0,0 +1,30 @@
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_CRFA crfa)
+
+FIND_PATH(
+    CRFA_INCLUDE_DIRS
+    NAMES crfa/api.h
+    HINTS $ENV{CRFA_DIR}/include
+        ${PC_CRFA_INCLUDEDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/include
+          /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    CRFA_LIBRARIES
+    NAMES gnuradio-crfa
+    HINTS $ENV{CRFA_DIR}/lib
+        ${PC_CRFA_LIBDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CRFA DEFAULT_MSG CRFA_LIBRARIES CRFA_INCLUDE_DIRS)
+MARK_AS_ADVANCED(CRFA_LIBRARIES CRFA_INCLUDE_DIRS)
+
---
+++ swig/crfa_swig.i
@@ -0,0 +1,13 @@
+/* -*- c++ -*- */
+
+#define CRFA_API
+
+%include "gnuradio.i"			// the common stuff
+
+//load generated python docstrings
+%include "crfa_swig_doc.i"
+
+%{
+%}
+
+
